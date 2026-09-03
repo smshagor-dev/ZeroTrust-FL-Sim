5ed26f1e0bc1f7bebc566b6edb9a22da981f7be4
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-09-04 02:45:12 +0300

    feat: export coordinated collusion attack

--- fl/zerotrust_fl/attacks/__init__.py
+++ fl/zerotrust_fl/attacks/__init__.py
@@ -4,9 +4,18 @@ from .poisoning import (
     AttackConfig,
     PoisoningAttack,
     adaptive_poison,
+    coordinated_collusion,
     gaussian_noise,
     label_flip,
     sign_flip,
 )
 
-__all__ = ["AttackConfig", "PoisoningAttack", "adaptive_poison", "gaussian_noise", "label_flip", "sign_flip"]
+__all__ = [
+    "AttackConfig",
+    "PoisoningAttack",
+    "adaptive_poison",
+    "coordinated_collusion",
+    "gaussian_noise",
+    "label_flip",
+    "sign_flip",
+]
