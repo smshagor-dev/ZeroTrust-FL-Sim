2c2e0fdb313e6bd71993fc7a415e6e1497b0a849
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-09-07 09:48:25 +0300

    style: group backend parity imports

--- tests/test_backend_parity.py
+++ tests/test_backend_parity.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import pytest
 import torch
+
 from zerotrust_fl.aggregators.native_cpp import CppByzantineAggregator
 from zerotrust_fl.engine.coordinator import AggregationConfig, _torch_aggregate
 
